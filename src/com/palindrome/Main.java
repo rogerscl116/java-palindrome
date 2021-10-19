6d7de74dd8f7a13a5fc9369535567b19c0116f67
charity.rogers <rogerscl116@gmail.com>
2021-10-18 20:44:49 -0500

    add main method code

--- src/com/palindrome/Main.java
+++ src/com/palindrome/Main.java
@@ -3,6 +3,27 @@ package com.palindrome;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // examples of palindromes: mom, dad, bob, madam, race car, nurses run
+
+        String original = "race car";
+        original = original.replace(" ", "");
+        String reverse = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reverse += original.charAt(i);
+        }
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < original.length(); i++) {
+            if (original.charAt(i) != reverse.charAt(i)) {
+                isPalindrome = false;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("It's a palindrome!");
+        } else {
+            System.out.println("It's NOT a palindrome!");
+        }
     }
 }
